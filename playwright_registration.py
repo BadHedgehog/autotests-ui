b619ce893146a0dd095a98ba207b2245f45e66f6
BadHedgehog <andrey.chem00@mail.ru>
2026-04-14 11:49:12 +0300

    SSH test

--- playwright_registration.py
+++ playwright_registration.py
@@ -1,24 +1,25 @@
 from playwright.sync_api import sync_playwright, expect
 
+
 with sync_playwright() as playwright:
     browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context(storage_state="browser-state.json") # Указываем файл с сохраненным состоянием
+    page = context.new_page()
 
-    page = browser.new_page()
-    page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
+    page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/dashboard")
+
+    page.wait_for_timeout(5000)
 
     email_input = page.get_by_test_id('registration-form-email-input').locator('input')
-    email_input.fill("user.name@gmail.com")
+    email_input.fill('user.name@gmail.com')
 
     username_input = page.get_by_test_id('registration-form-username-input').locator('input')
-    username_input.fill("username")
+    username_input.fill('username')
 
     password_input = page.get_by_test_id('registration-form-password-input').locator('input')
-    password_input.fill("password")
+    password_input.fill('password')
 
     registration_button = page.get_by_test_id('registration-page-registration-button')
     registration_button.click()
 
-    heading = page.get_by_test_id("dashboard-toolbar-title-text")
-    expect(heading).to_be_visible()
-    expect(heading).to_have_text("Dashboard")
-    page.wait_for_timeout(5000)
+    context.storage_state(path="browser-state.json")
